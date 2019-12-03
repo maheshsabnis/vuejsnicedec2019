93b5c1e7d6db22a280130a09a88a67790e703ab0
Mahesh Sabnis <sabnis_m@hotmail.com>
2019-12-03 11:32:40 +0530

    Add files via upload

---
+++ pccommunication/ChildComponent.vue
@@ -0,0 +1,45 @@
+<template>
+   <div id="dvChild">
+       <h2>I am Child Component</h2>
+       <input type="button" value="Send Message" @click="display">
+       <div>
+           <span>
+               {{message}}
+           </span>
+       </div>
+   </div>
+</template>
+
+<script>
+// the child component uses the 'props' object to define
+// input binding properties aka properties uses across components
+// for communication. E.g. when the component is used inside the parent
+// input binding will be used as v-bind:<PROPS-NAME>, v-bind:message
+export default {
+    name: 'ChildComponent',
+    props:{
+        // property map for the props so that mounting can initialize the value
+        message:String 
+    },
+    created(){
+        console.log('Component Created');
+    },
+    mounted(){
+        console.log('Component Mounted');
+    },
+    data: function(){
+        return {
+            sendMessage:''
+        }
+    },
+    methods:{
+        display: function(){
+            alert('Message from Child Component');
+            this.sendMessage = 'Hay Parent, I am comming from child...';
+            // the 'notify' is an event that will be emitted by the
+            // current component
+            this.$emit('notify',this.sendMessage);
+        }
+    }
+}
+</script>
---
+++ pccommunication/ParentComponent.vue
@@ -0,0 +1,34 @@
+<template>
+   <div id="dvParent">
+       <h2>I am Parent Component</h2>
+       <input type="button" value="Change Message" @click="updateMessage()">
+       <ChildComponent v-bind:message="msg" @notify="onReceiveMessage"></ChildComponent>
+       <hr>
+       <p>Data Received from Child</p>
+       <div>{{receivedMessage}}</div>   
+   </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vue';
+export default {
+    name: 'ParentComponent',
+    components:{
+        ChildComponent
+    },
+    data: function(){
+        return {
+            msg: 'The Message from Parent',
+            receivedMessage: ''
+        }
+    },
+    methods:{
+        onReceiveMessage: function(m){
+            this.receivedMessage = m;
+        },
+        updateMessage:function(){
+            this.msg = 'New Message';
+        }
+    }
+}
+</script>
